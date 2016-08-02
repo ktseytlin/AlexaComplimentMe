7847e6914771dee03445b047a8539e189f40b01c
Keren Tseytlin <keren.tseytlin@capitalone.com>
2016-08-02 09:31:14 -0400

    semi working version

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Amazon Alexa project that will give you a compliment.
 
 TODO: Finish up README.
+
+### To Upload to Lambda
+
+Right click on ask folder, lambda_function.py, and compliments.txt file, and then compress. Upload the compressed zip to Lambda.
+
+ASK folder put in by using Method 1 - git cloning and then taking out everything but that one folder and moving that folder into root.
--- lambda_function.py
+++ lambda_function.py
@@ -1,28 +1,26 @@
 from ask import alexa
 from random import randint
 
-def lambda_handler(request_obj, context=None):
-    ''' All requests start here '''
+def lambda_handler(request_obj, context={}):
     return alexa.route_request(request_obj)
 
-@alexa.default
-def default_handler(request):
-    """ The default handler gets invoked if no handler is set for a request """
+@alexa.default_handler()
+def default_handler():
     return alexa.create_response(message="Just say Alexa compliment me!")
 
-@alexa.request("LaunchRequest")
+@alexa.request_handler("LaunchRequest")
 def launch_request_handler(request):
     return alexa.create_response(message="Welcome to Compliment Me! On your way out to conquer the day "
                                  "or just need a pick me up? Boost yourself with a compliment!",
-                                 reprompt_message='Would you like a compliment?'
+                                 reprompt_message='Would you like a compliment? '
                                  'Say, Alexa Compliment Me.')
 
-@alexa.request(request_type="SessionEndedRequest")
+@alexa.request_handler(request_type="SessionEndedRequest")
 def session_ended_request_handler(request):
     return alexa.create_response(message="Bye!")
 
-@alexa.intent('GetComplimentIntent')
-def get_compliments(request):
+@alexa.intent_handler("GetComplimentIntent")
+def get_compliment_intent_handler(request):
     linesOfCompliments = [line.rstrip('\n') for line in open('compliments.txt')]
 
     randCompliment = (randint(0,len(linesOfCompliments)-1))
@@ -30,10 +28,10 @@ def get_compliments(request):
 
     return alexa.create_response(message=compliment, end_session=True)
 
-@alexa.intent('AMAZON.HelpIntent')
+@alexa.intent_handler("AMAZON.HelpIntent")
 def help_intent_handler(request):
-    return alexa.create_response(message="You can ask for a compliment by saying alexa compliment me!")
+    return alexa.create_response(message="This skill compliments you when you say alexa compliment me!")
 
-@alexa.intent('AMAZON.StopIntent')
+@alexa.intent_handler("AMAZON.StopIntent")
 def stop_intent_handler(request):
     return alexa.create_response(message="Bye!")
