9f66ddc1a4b36ba30d793ddb83bdd44791e25ba8
Keren Tseytlin <keren.tseytlin@capitalone.com>
2017-02-07 22:15:32 -0500

    fun facts pushed

--- lambda_function.py
+++ lambda_function.py
@@ -10,27 +10,25 @@ def default_handler(request):
 
 @alexa.request_handler("LaunchRequest")
 def launch_request_handler(request):
-    return alexa.create_response(message="Welcome to Sweet and Sour! On your way out to conquer the day "
-                                 "or just need a pick me up? Boost yourself with a compliment!",
-                                 reprompt_message='Would you like a compliment? '
-                                 'Say, compliment Me.')
+    return alexa.create_response(message="Welcome to Fun Fact!",
+                                 reprompt_message='Would you like a fun fact?')
 
 @alexa.request_handler(request_type="SessionEndedRequest")
 def session_ended_request_handler(request):
     return alexa.create_response(message="Bye!", end_session=True)
 
-@alexa.intent_handler("GetComplimentIntent")
-def get_compliment_intent_handler(request):
-    linesOfCompliments = [line.rstrip('\n') for line in open('compliments.txt')]
+@alexa.intent_handler("GetFactIntent")
+def get_fact_intent_handler(request):
+    linesOfFacts = [line.rstrip('\n') for line in open('facts.txt')]
 
-    randCompliment = (randint(0,len(linesOfCompliments)-1))
-    compliment = linesOfCompliments[randCompliment]
+    randFact = (randint(0,len(linesOfFacts)-1))
+    fact = linesOfFacts[randFact]
 
-    return alexa.create_response(message=compliment, end_session=True)
+    return alexa.create_response(message=fact, end_session=True)
 
 @alexa.intent_handler("AMAZON.HelpIntent")
 def help_intent_handler(request):
-    return alexa.create_response(message="This skill compliments you when you say compliment me!", end_session=False)
+    return alexa.create_response(message="This skill tells you a fun fact!", end_session=False)
 
 @alexa.intent_handler("AMAZON.StopIntent")
 def stop_intent_handler(request):
